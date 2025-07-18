af3aedad55d0a59b1eac8e80b29e40e7decb424c
QuispeRonny <ronny.quispe@udla.edu.ec>
2025-07-17 22:41:47 -0500

    agregamos carpeta con clase repository- agreamos imagen en  about page e informacion personal

--- MauiApp4/MauiProgram.cs
+++ MauiApp4/MauiProgram.cs
@@ -16,7 +16,7 @@ namespace MauiApp4
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
---
+++ MauiApp4/Repositories/FilesRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MauiApp4.Repositories
+{
+    internal class FilesRepository
+    {
+        private string filePath = Path.Combine(FileSystem.AppDataDirectory, "Archivo1.txt");
+
+    
+        public async Task<bool> GenerarArchivoAsync(string texto)
+        {
+            try
+            {
+                await File.WriteAllTextAsync(filePath, texto);
+                return File.Exists(filePath);  
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine($"Error al generar el archivo: {ex.Message}");
+                return false;
+            }
+        }
+
+        
+        public async Task<string> DevuelveInformacionArchivoAsync()
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string texto = await File.ReadAllTextAsync(filePath);
+                    return texto;
+                }
+                return "No existe el archivo";  
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine($"Error al leer el archivo: {ex.Message}");
+                throw; 
+            }
+        }
+    }
+}
